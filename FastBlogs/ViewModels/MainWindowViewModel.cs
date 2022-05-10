0b92a4ca2d4a47716cc27dd02ce2cbd77cee87e8
CoastRed <Coastline.alex7315@gmail.com>
2022-05-10 20:30:15 +0800

    添加项目文件。

---
+++ FastBlogs/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FastBlogs.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private readonly IRegionManager regionManager;
+       
+        #region 构造函数
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+
+        #endregion
+
+        #region 命令
+
+        public DelegateCommand<string> NavigateCommand
+        {
+            get
+            {
+                return new DelegateCommand<string>(viewName =>
+                {
+                    this.regionManager.Regions["MainWindowRegion"].RequestNavigate(viewName);
+                });
+            }
+        }
+
+        public DelegateCommand ExitCommand
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    Application.Current.Shutdown();
+                });
+            }
+        }
+
+        #endregion
+    }
+}
